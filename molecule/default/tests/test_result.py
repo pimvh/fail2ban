172097a6be764832e8e1f34334a6e08ede5f52cc
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2023-08-22 19:00:59 +0200

    test: remove obsolete test, rewrite comment, now should run on debian (#4)
    * test: remove obsolete test, rewrite comment, now should run on debian
    
    * fix: add correct privilege
    
    * fix: add privilege for systemd service too

--- molecule/default/tests/test_result.py
+++ molecule/default/tests/test_result.py
@@ -1,19 +1,14 @@
 import testinfra
 
 
-def test_os_release(host):
-    """test host release for good measure"""
-
-    assert host.file("/etc/os-release").contains("Ubuntu")
-
-
-def test_nsd_running(host):
+def test_fail2ban_running(host):
     """test that the fail2ban service is running"""
 
-    assert host.service("fail2ban").is_running
+    with host.sudo():
+        assert host.service("fail2ban").is_running
 
 
-def test_nsd_zone_file_created(host):
+def test_fail2ban_file_created(host):
     """test that the fail2ban jail.local file is created"""
-
-    assert host.file("/etc/fail2ban/jail.local")
+    with host.sudo():
+        assert host.file("/etc/fail2ban/jail.local")
